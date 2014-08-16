71510297a56183aa9a791af22edb107546a84faf
tmerlet <tmerlet@gmail.com>
2014-08-16 15:54:12 +0100

    added seed data, including images uploaded with carrierwave

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-p1 = Photo.create(description: "description 1", location: "location 1 ", image: "image 1")
-p2 = Photo.create(description: "description 2", location: "location 2 ", image: "image 2")
-p3 = Photo.create(description: "description 3", location: "location 3 ", image: "image 3")
-p4 = Photo.create(description: "description 3", location: "location 3 ", image: "image 3")
+photo1 = Photo.create(description: "description 1", location: "location 1 ", image: "image 1")
+photo2 = Photo.create(description: "description 2", location: "location 2 ", image: "image 2")
+photo3 = Photo.create(description: "description 3", location: "location 3 ", image: "image 3")
+photo4 = Photo.create(description: "description 3", location: "location 3 ", image: "image 3")
 
+User1 = User.create(name: "Piers", email: "piers@ga.com", password: "password", password_confirmation: "password", dob: "12/04/2012", profile_image: File.open(File.join(Rails.root.join('public'), 'piersprofilepic.jpg')))
+
+User2 = User.create(name: "Alex", email: "alex@ga.com", password: "password", password_confirmation: "password", dob: "12/04/2012", profile_image: File.open(File.join(Rails.root.join('public'), 'alexprofilepic.jpg')))
+
+User3 = User.create(name: "Toby", email: "alex@ga.com", password: "password", password_confirmation: "password", dob: "12/04/2012", profile_image: File.open(File.join(Rails.root.join('public'), 'tobyprofilepic.jpg'))) 
