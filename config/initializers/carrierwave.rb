7b3e1e08e34551f707abdd45d1bca99869102289
Alex Windett <alexwindett@hotmail.com>
2014-08-17 09:01:29 +0100

    changed aws keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider  => 'AWS',  # required
-    :aws_access_key_id  => 'AKIAIXO52E2KNCFLP4XA',  # required
-    :aws_secret_access_key  => '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',  # required
-    :region => 'eu-west-1',  # optional, defaults to 'us-east-1'
+    :aws_access_key_id  => ENV['aws_access_key_id'],  # required
+    :aws_secret_access_key => ENV['aws_secret_access_key'],  # required
+    :region  => 'eu-west-1',  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'photoapp-wdi7' ,  # required
+  config.fog_directory  = ENV['WDI_S3_BUCKET']  # required
   config.fog_public  = true  # optional, defaults to true
 end
