ac7484ac9d00d3996469807efae68b5137648bb0
tmerlet <tmerlet@gmail.com>
2014-08-19 16:11:32 +0100

    refactor code

--- app/assets/javascripts/ratings.js
+++ app/assets/javascripts/ratings.js
@@ -14,55 +14,64 @@ function unlikeRating(){
   $numberOfUnlikes = parseInt($('#number_of_unlikes').text())
   
   if ($this.children("i").hasClass("fa-thumbs-o-down")){
-    request("POST", "/ratings", {
-      rating:{
-        photo_id: $("#currentphoto").val(),
-        status: "negative"
-      }
-    }).success(function(data){
-      $this.children("i").data("rating-id", data.id);
-      $('#number_of_unlikes').text($numberOfUnlikes + 1)
-    })
+    addUnlikeRating();
   }
-
   if ($this.children("i").hasClass("fa-thumbs-down")){
-    //  debugger
-    ratingId = $this.children("i").data("rating-id");
-    request("DELETE", "/ratings/"+ratingId, null).success(function(data){
-      $('#number_of_unlikes').text($numberOfUnlikes - 1)
-    })
+    removeUnlikeRating();
   }
-
   $this.children("i").toggleClass("fa-thumbs-o-down").toggleClass("fa-thumbs-down")
-
 }
 
+function addUnlikeRating(){
+  request("POST", "/ratings", {
+    rating:{
+      photo_id: $("#currentphoto").val(),
+      status: "negative"
+    }
+  }).success(function(data){
+    $this.children("i").data("rating-id", data.id);
+    $('#number_of_unlikes').text($numberOfUnlikes + 1);
+  })
+}
+
+function removeUnlikeRating(){
+  ratingId = $this.children("i").data("rating-id");
+  request("DELETE", "/ratings/"+ratingId, null).success(function(data){
+    $('#number_of_unlikes').text($numberOfUnlikes - 1)
+  })
+}
+   
 function likeRating(){
   $this = $(this)
   $numberOfLikes = parseInt($('#number_of_likes').text())
   
   if ($this.children("i").hasClass("fa-thumbs-o-up")){
-    request("POST", "/ratings", {
-      rating:{
-        photo_id: $("#currentphoto").val(),
-        status: "positive"
-      }
-    }).success(function(data){
-      $this.children("i").data("rating-id", data.id);
-      $('#number_of_likes').text($numberOfLikes + 1)
-    })
+    addLikeRating();
   }
 
   if ($this.children("i").hasClass("fa-thumbs-up")){
-    //  debugger
-    ratingId = $this.children("i").data("rating-id");
-    request("DELETE", "/ratings/"+ratingId, null).success(function(data){
-      $('#number_of_likes').text($numberOfLikes - 1)
-    })
+    removeLikeRating();
   }
-
   $this.children("i").toggleClass("fa-thumbs-o-up").toggleClass("fa-thumbs-up")
+}
 
+function addLikeRating(){
+  request("POST", "/ratings", {
+    rating:{
+      photo_id: $("#currentphoto").val(),
+      status: "positive"
+    }
+  }).success(function(data){
+    $this.children("i").data("rating-id", data.id);
+    $('#number_of_likes').text($numberOfLikes + 1)
+  })
+}
+
+function removeLikeRating(){
+  ratingId = $this.children("i").data("rating-id");
+  request("DELETE", "/ratings/"+ratingId, null).success(function(data){
+    $('#number_of_likes').text($numberOfLikes - 1)
+  })
 }
 
 
