a60ed6d2f8c3f1fb8e58d062d5d7c5326f0c6d91
tmerlet <tmerlet@gmail.com>
2014-08-20 18:16:27 +0100

    refactor code

--- app/assets/javascripts/webcam.js
+++ app/assets/javascripts/webcam.js
@@ -1,6 +1,7 @@
 function onFailure(err) {
     alert("The following error occured: " + err.name);
 }
+
 jQuery(document).ready(function () {
   if(window.user){
     var video = document.querySelector('#webcam');
@@ -9,13 +10,9 @@ jQuery(document).ready(function () {
                       navigator.mozGetUserMedia ||
                       navigator.msGetUserMedia);
     if (navigator.getUserMedia) {
-        navigator.getUserMedia
-                      (
-                        { video: true },
-                        function (localMediaStream) {
-                            video.src = window.URL.createObjectURL(localMediaStream);
-
-                        }, onFailure);
+        navigator.getUserMedia({ video: true }, function (localMediaStream) {
+          video.src = window.URL.createObjectURL(localMediaStream);
+        }, onFailure);
     }
     else {
         alert('OOPS No browser Support');
@@ -28,43 +25,12 @@ jQuery(document).ready(function () {
     button.addEventListener('click',snapshot, false);
      
     function snapshot() {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    ctx.drawImage(video, 0, 0);
+
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      //  canvas.width = 300 didn't work the canvas was that size but the image was badly cropped
+      // 
+      ctx.drawImage(video, 0, 0);
     }
   }
 });
-
-// jQuery(document).ready(function () {
-//   if(window.photo){
-//     var video = document.querySelector('#webcam');
-//     navigator.getUserMedia = (navigator.getUserMedia ||
-//                       navigator.webkitGetUserMedia ||
-//                       navigator.mozGetUserMedia ||
-//                       navigator.msGetUserMedia);
-//     if (navigator.getUserMedia) {
-//         navigator.getUserMedia
-//                       (
-//                         { video: true },
-//                         function (localMediaStream) {
-//                             video.src = window.URL.createObjectURL(localMediaStream);
-
-//                         }, onFailure);
-//     }
-//     else {
-//         alert('OOPS No browser Support');
-//     }
-
-//     var button = document.querySelector('#screenshot-button');
-//     var canvas = document.querySelector('#screenshot-canvas');
-//     var ctx = canvas.getContext('2d');
-     
-//     button.addEventListener('click',snapshot, false);
-     
-//     function snapshot() {
-//     canvas.width = video.videoWidth;
-//     canvas.height = video.videoHeight;
-//     ctx.drawImage(video, 0, 0);
-//     }
-//   }
-// });
