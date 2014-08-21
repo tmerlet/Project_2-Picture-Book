e485d70a4b9a8124ed4591c1e5f1c50876218f56
Piers Karpinski <piers.karpinski@gmail.com>
2014-08-21 07:35:32 +0100

    added role to seed data, about to drop database

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :profile_image, :name, :dob
+  attr_accessible :role, :email, :password, :password_confirmation, :remember_me, :profile_image, :name, :dob
   # attr_accessible :title, :body
 
   mount_uploader :profile_image,
@@ -19,5 +19,5 @@ class User < ActiveRecord::Base
   def role?(role)
     self.role.to_s == role.to_s
   end
-  
+
 end
