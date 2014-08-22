c67eeb3de01b3b5ffc9fd2eafd581f26531fd078
tmerlet <tmerlet@gmail.com>
2014-08-22 09:51:42 +0100

    fix to the edit user after path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,10 +10,24 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def update
-    super
 
-    @user.profile_image = uploaded_picture(params[:canvasimage])
-    @user.save
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+        prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+        resource_updated = update_resource(resource, account_update_params)
+        yield resource if block_given?
+        if resource_updated
+          if is_flashing_format?
+            flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+              :update_needs_confirmation : :updated
+            set_flash_message :notice, flash_key
+          end
+          sign_in resource_name, resource, bypass: true
+          respond_with resource, location: users_path
+        else
+          clean_up_passwords resource
+          respond_with resource
+        end
   end
 
   def uploaded_picture base_64_string
