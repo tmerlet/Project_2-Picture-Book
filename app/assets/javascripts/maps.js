e67fefc91df6773369c7d8de487a6c5d3c3c1167
tmerlet <tmerlet@gmail.com>
2014-08-22 05:59:04 +0100

    documentation update toby

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,7 +1,11 @@
+
+// this file contains the javascript required to show the map on the photo show page.  -TM
+
+//  window.photo stops this code from running unless it is on the photo - show page. - TM
 $(function(){
   if(window.photo){
   
-
+// this function creates the market pointing to specific coordinates. -TM
     function marker(myLatlng, map){
       marker = new google.maps.Marker({
          position: myLatlng,
@@ -12,7 +16,7 @@ $(function(){
     function initialize() {
       var map;
 
-
+//  if no coordinates are passed in, use generic coordinates. -TM
       if(!(window.photo.latitude && window.photo.longitude)){
         var myLatlng = new google.maps.LatLng(48.0658107, -0.1015987);
         var zoomlevel = 1
@@ -25,9 +29,10 @@ $(function(){
         center: myLatlng,
         zoom: zoomlevel
       };
-      
-      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions); 
 
+// draw a map on the canvas -TM
+      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions); 
+// if we have spefic coordinates, create a marker.  -TM
       if((window.photo.latitude && window.photo.longitude)) {
         marker(myLatlng, map);
       }
--- app/assets/javascripts/webcam.js
+++ app/assets/javascripts/webcam.js
@@ -1,9 +1,13 @@
+// This Javascript file controles the webcam on the signup for.  you can view the form in devise, registration new and edit.  
+
+
 var imageString = null;
 
 function onFailure(err) {
     alert("The following error occured: " + err.name);
 }
 
+// once the page has loaded, checks to see whether the browser supports video. 
 jQuery(document).ready(function () {
   if(window.user){
     var video = document.querySelector('#webcam');
@@ -20,24 +24,26 @@ jQuery(document).ready(function () {
         alert('OOPS No browser Support');
     }
 
+// defines some variables that refer to objects on the page.  
     var button = document.querySelector('#screenshot-button');
     var webbutton = document.querySelector('#showwebcam-button');
     var canvas = document.querySelector('#screenshot-canvas');
     var ctx = canvas.getContext('2d');
-     
+
+// defines what happens when the 'capture' button is clicked     
     function snapshot(event) {
       event.preventDefault();
 
+// swaps between the webcam and screenshot
       $('#screenshot-canvas').toggleClass('dispnone')
       $('#webcam').toggleClass('dispnone')
 
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
-      //  canvas.width = 300 didn't work the canvas was that size but the image was badly cropped
-      // 
+
       ctx.drawImage(video, 0, 0);
       imageString = canvas.toDataURL('image/png')
-      console.log("imageString", imageString)
+      // console.log("imageString", imageString)
       $('#canvasimage').attr('value', imageString)
 
 
@@ -46,11 +52,11 @@ jQuery(document).ready(function () {
       // document.querySelector('img').src = canvas.toDataURL('image/webp');
     }
 
+// shows the webcam
     function showwebcam(event){
       event.preventDefault();
       $('#video').toggleClass('dispnone')
     }
-
 
     button.addEventListener('click',snapshot, false);
     webbutton.addEventListener('click',showwebcam, false);
